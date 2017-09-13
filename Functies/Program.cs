49607dfc5b8fb6ab59098f1e5b8f93fef2820033
Boy Kortman <b.kortman@leeuwenborgh.nl>
2017-09-13 11:53:55 +0200

    Neiuwe functie toegevoegd en gebruikt in de main, gebruiker de mogelijkheid gegeven om multiplier aan te passen.

--- Functies/Program.cs
+++ Functies/Program.cs
@@ -10,6 +10,29 @@ namespace Functies
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Dit is een uitleg over functies, typ iets in...");
+            int intResult = Program.ShowConsoleInfo(Console.ReadLine(), 10);
+            Console.WriteLine("De uitkomst van de functie is : " + intResult);
+            Console.ReadLine();
         }
+
+        static int ShowConsoleInfo(string strResult, int intGetal, int intMultiplier = 5)
+        {
+            Console.WriteLine("U heeft ingetikt : " + strResult + " het getal is : " + intGetal);
+            Console.WriteLine("Wilt u de multiplier aanpassen ? (y/n)");
+            string strAdjustMultiplier = Console.ReadLine();
+
+            if (strAdjustMultiplier == "y")
+            {
+                Console.WriteLine("Voer uw nieuwe multiplier in : ");
+                intMultiplier = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            Console.ReadLine();
+
+            return (intGetal * intMultiplier);
+        }
+
+        //return value
     }
 }
